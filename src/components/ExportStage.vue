c749dd8b66cd2882ad3e197e23dcb00bd498d110
JS <jsd1231@hotmail.fr>
2021-12-01 21:04:07 +0100

    export

--- src/components/ExportStage.vue
+++ src/components/ExportStage.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="export">
+    <div>
+      <label>Exporter depuis cette date :</label>
+      <input type="date" v-model="critere.date"/>
+    </div>
+    <div>
+      <label>Inclure les stages non-validés ?</label>
+      <input type="checkbox" v-model="critere.nonValid"/>
+    </div>
+    <div>
+      <label>Inclure les stages déja exportés ?</label>
+      <input type="checkbox" v-model="critere.alryExport"/>
+    </div>
+    <div>
+      <label>Inclure les stages dont la convention entreprise est déjà signée ?</label>
+      <input type="checkbox" v-model="critere.convEntre"/>
+    </div>
     <input type="button" value="Exporter" @click="doExport" />
   </div>
 </template>
@@ -8,17 +24,45 @@
 import axios from "axios";
 export default {
   name: "ExportStage",
+  data(){
+    return{
+      critere:{
+        date: new Date(Date.now()).toISOString().substring(0, 10),
+        nonValid: false,
+        alryExport: false,
+        convEntre: false
+      }
+    }
+  },
   methods: {
     doExport: function () {
+      if(this.critere.date){
       axios
-        .get("http://localhost/convention/exportStage.php")
-        .then(function () {})
+        .post("http://localhost/convention/exportStage.php",this.critere)
+        .then(function (response) {
+          console.log(response);
+          axios.get("http://localhost/convention/export.csv")
+          .then(function (responseFile){
+            var fileURL = window.URL.createObjectURL(new Blob([responseFile.data]));
+            var fileLink = document.createElement('a');
+
+            fileLink.href = fileURL;
+            fileLink.setAttribute('download', 'export.csv');
+            document.body.appendChild(fileLink);
+
+            fileLink.click();
+          })
+          .catch(function () {})
+        })
         .catch(function () {});
+    }
     },
+    
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
